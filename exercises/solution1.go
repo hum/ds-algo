86807f09a930591381acd8007705892bfe095755
hum <matonohamartin@gmail.com>
2021-01-14 12:50:55 +0100

    rename files to match the problems in README

--- exercises/solution1.go
+++ exercises/solution1.go
@@ -1,39 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	result := mergeSortedArrays([]int{0, 3, 4, 31}, []int{4, 6, 30})
+	result := reverse("Hello, my name is...")
 	fmt.Println(result)
 }
 
-func mergeSortedArrays(arr1, arr2 []int) []int {
-	result := make([]int, 0, len(arr1)+len(arr2))
-	index1, index2 := 0, 0
+func reverse(s string) string {
+	length := len(s)
+	runes := make([]rune, length)
 
-	for {
-		if index1 == len(arr1) {
-			result = append(result, arr2[index2:]...)
-			break
-		}
-
-		if index2 == len(arr2) {
-			result = append(result, arr1[index1:]...)
-			break
-		}
-
-		value := 0
-		element1, element2 := arr1[index1], arr2[index2]
-
-		if element1 < element2 {
-			value = element1
-			index1++
-		} else {
-			value = element2
-			index2++
-		}
-
-		result = append(result, value)
+	for _, v := range s {
+		length--
+		runes[length] = v
 	}
-	return result
+	return string(runes)
 }
