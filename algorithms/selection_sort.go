2ed30bb171586de7d67857a39635b8ccccc1c977
hum <matonohamartin@gmail.com>
2021-01-26 14:56:51 +0100

    go fmt

--- algorithms/selection_sort.go
+++ algorithms/selection_sort.go
@@ -4,9 +4,9 @@ func SelectionSort(values []int) []int {
 	for i := 0; i < len(values); i++ {
 		min := i
 
-		for j := i+1; j < len(values); j++ {
+		for j := i + 1; j < len(values); j++ {
 			if values[j] < values[min] {
-				min = j 
+				min = j
 			}
 		}
 		values[i], values[min] = values[min], values[i]
--- algorithms/selection_sort_test.go
+++ algorithms/selection_sort_test.go
@@ -1,13 +1,13 @@
 package algorithms_test
 
 import (
-  "testing"
+	"testing"
 
-  algo "github.com/hum/ds-algo/algorithms"
+	algo "github.com/hum/ds-algo/algorithms"
 )
 
 func TestSelectionSort(t *testing.T) {
-  values := []int{99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0}
+	values := []int{99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0}
 	expected := []int{0, 1, 2, 4, 5, 6, 44, 63, 87, 99, 283}
 
 	algo.SelectionSort(values)
